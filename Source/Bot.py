226bdee8299d510e8f74c27b7a65debd01ce1d6e
Tom Burniston <tomburniston@gmail.com>
2022-04-09 14:02:44 -0700

    Initial Push
    push

---
+++ Source/Bot.py
@@ -0,0 +1,25 @@
+import discord.py
+
+
+
+import discord
+
+
+
+
+    
+
+@client.event
+async def on_ready():
+    client.loop.create_task(benScan())
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('TOKEN')
